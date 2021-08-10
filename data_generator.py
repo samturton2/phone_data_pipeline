7d74e053f64c540ed45eb2737d472597e7e1a445
samturton2 <STurton@spartaglobal.com>
2021-08-10 11:45:07 +0100

    ran if name = main

--- data_generator.py
+++ data_generator.py
@@ -6,22 +6,31 @@ headers = ['country', 'phone_number']
 
 def populate_csv():
 
+	# Open input csv file and write over it
 	with open('/input.csv', 'w') as f:
 		writer = csv.writer(f)
 
+		# insert headers
 		writer.writerow(headers)
 
+		# Insert 1000 rows of data
 		for i in range(1000):
+			
 			temp_data = []
+			# Get random country from list
 			temp_data.append(countries[r.randint(0,2)])
 			
-			ph_no = []
-			ph_no.append(r.randint(6, 9))
+			# create phone number string
+			ph_no = ''
+			ph_no.append(str(r.randint(6, 9)))
 			for i in range(1, 10):
-			    ph_no.append(r.randint(0, 9))
+			    ph_no.append(str(r.randint(0, 9)))
+			# write to temp data 
+			temp_data.append(ph_no)
 
-			temp_data.append("".join(ph_no))
+			writer.writerow(temp_data)
 
 
 
-
+if __name__ == '__main__':
+	populate_csv()
