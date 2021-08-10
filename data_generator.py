0841468b14c9489a18fca7e4dfd898baceba2547
samturton2 <STurton@spartaglobal.com>
2021-08-10 12:27:02 +0100

    data_generator_complete wit randomise

--- data_generator.py
+++ data_generator.py
@@ -5,6 +5,23 @@ import pandas as pd
 countries = ['USA', 'UK', 'FRANCE', 'SPAIN']
 headers = ['country', 'phone_number']
 
+def randomise_num(ph_no):
+	if r.randint(0,13)%6 == 0:
+		# Get in format XXXXX XXX XXX
+		return f"0{ph_no[0:4]} {ph_no[4:7]} {ph_no[7:]}"
+	elif r.randint(0,15)%5 == 0:
+		# Get in format (XXXX) XXX XXX
+		return f"({ph_no[0:4]}) {ph_no[4:7]} {ph_no[7:]}"
+	elif r.randint(1,5)%3 == 0:
+		# Get in format XXXX-XXX-XXX
+		return f"{ph_no[0:4]}-{ph_no[4:7]}-{ph_no[7:]}"
+	else
+		return ph_no
+
+
+
+
+
 def populate_csv():
 
 	# Open input csv file and write over it
@@ -26,6 +43,10 @@ def populate_csv():
 			ph_no += str(r.randint(6, 9))
 			for i in range(1, 10):
 			    ph_no += str(r.randint(0, 9))
+
+			# Randomise data
+			ph_no = randomise_num(ph_no)
+
 			# write to temp data 
 			temp_data.append(ph_no)
 
